2970336594dd0474a2862ed57a50ebc50376f043
h15200 <45977354+h15200@users.noreply.github.com>
2019-10-01 12:44:34 -0700

    finished test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, signIn} from './actions/index'
 
 function App() {
+
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+  const handleIncrement = () => {
+    dispatch(increment())
+  }
+  const handleDecrement = () => {
+    dispatch(decrement())
+  }
+  const handleSignIn = () => {
+    dispatch(signIn())
+  }
+
   return (
-    <div className="App">
-      Hello React
+    <div>
+      <h1 className="App">
+        Counter: {counter}
+      </h1>
+      <button style={{fontSize:'2rem', margin:'0 2em'}} onClick={handleIncrement}>+</button>
+      <button style={{fontSize: '2rem', margin: '0 2em'}} onClick={handleDecrement}>-</button>
+      <button style={{fontSize: '2rem', margin: '0 2em'}} onClick={handleSignIn}>Sign In</button>
+      {isLogged && <h2>Secret Info that you need to be logged in for!</h2>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import allReducers from './reducers/index'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
+//                        REDUX without react-redux
+
+//Steps in redux:
+  // import createstore
+  // make action, then reducer
+  // let store = createstore(reducer)
+  // now you can use store.getState(), store.subscribe(), store.disptach(action)
+
+
+//Action
+
+// const increment = () => {
+//   return {type: 'INCREMENT'}
+// }
+
+// const decrement = () => {
+//   return { type: 'DECREMENT'}
+// }
+
+// // Reducer
+
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1
+//     case 'DECREMENT':
+//       return state - 1
+//     default:
+//       return state
+//   }
+// }
+
+// let store = createStore(counter)
+
+// store.subscribe(()=> console.log(store.getState()))
+
+// store.dispatch(increment())
+// store.dispatch(increment())
+// store.dispatch(decrement())
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+     <App />
+  </Provider>, document.getElementById('root'));
 
